f35800d8477a909938844d548149f504bf9bfbd6
Abu Bakar Wase <33964884+abubakarwase@users.noreply.github.com>
2020-04-05 23:13:45 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+const errorHandler = require("./middlewares/error");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to Database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
+const app = express();
+
+// req body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
